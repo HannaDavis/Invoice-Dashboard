c966190a88b0ea603e5c75a65bca9230e6d8c818
Hanna Davis <hanna.davis@troyweb.com>
2021-04-17 16:18:12 -0400

    Initial Commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+    <breeze-authenticated-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+    </breeze-authenticated-layout>
+</template>
+
+<script>
+    import BreezeAuthenticatedLayout from '@/Layouts/Authenticated'
+
+    export default {
+        components: {
+            BreezeAuthenticatedLayout,
+        },
+    }
+</script>
